fbb49d2b42895c742a8a914393a2700daf423f8e
ALAN PLAUN <47165921@ort>
2022-10-04 11:34:04 -0300

    Creacion del proyecto. Falta subir gitignore

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace tp09proyectofinal.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
