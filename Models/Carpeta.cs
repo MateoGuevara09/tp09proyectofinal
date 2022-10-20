a6b7d79280dff70fba25fc6c4f7f678cacb41617
ALAN PLAUN <47165921@ort>
2022-10-20 09:01:21 -0300

    Creacion BD

--- Models/Carpeta.cs
+++ Models/Carpeta.cs
@@ -1,9 +1,9 @@
 namespace tp09proyectofinal.Models;
 
-public class carpeta{
+public class Carpeta{
     private int _idCarpeta;private string _idUsuario;private string _NombreCarpeta;
-    public carpeta(){}
-    public carpeta(int pidCarpeta,string pidUsuario,string pNombreCarpeta){
+    public Carpeta(){}
+    public Carpeta(int pidCarpeta,string pidUsuario,string pNombreCarpeta){
         _idCarpeta = pidCarpeta;
         _idUsuario = pidUsuario;
         _NombreCarpeta = pNombreCarpeta;
